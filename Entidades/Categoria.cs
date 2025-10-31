d9aff21cfaabb8678077db41d04230771ef54664
Eduardo M.L <em599554@gmail.com>
2025-10-31 12:26:03 -0600

    Se agregaron las Entidades y sus constructores.

---
+++ Entidades/Categoria.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Categoria
+    {
+        public Categoria(int idCategoria, string nombre, string descripcion)
+        {
+            IdCategoria = idCategoria;
+            Nombre = nombre;
+            Descripcion = descripcion;
+        }
+
+        public int IdCategoria { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
---
+++ Entidades/Proveedor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Proveedor
+    {
+        public Proveedor(int idProveedor, string nombre, string contacto, string telefono, string correo, string direccion, bool activo)
+        {
+            IdProveedor = idProveedor;
+            Nombre = nombre;
+            Contacto = contacto;
+            Telefono = telefono;
+            Correo = correo;
+            Direccion = direccion;
+            Activo = activo;
+        }
+
+        public int IdProveedor { get; set; }
+        public string Nombre { get; set; }
+        public string Contacto { get; set; }
+        public string Telefono { get; set; }
+        public string Correo { get; set; }
+        public string Direccion { get; set; }
+        public bool Activo { get; set; }
+    }
+}
