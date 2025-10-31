d834aacd842c66f388c9500a329d391c25ad713b
FerDavalosHz <fernadodavalos2005@gmail.com>
2025-10-31 12:33:21 -0600

    Merge de todas las entidades

---
+++ Entidades/Producto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Producto
+    {
+        public Producto(int idProducto, string nombre, string descripcion, decimal precioVentaActual, int stockMinimo, bool activo, int idCategoria)
+        {
+            IdProducto = idProducto;
+            Nombre = nombre;
+            Descripcion = descripcion;
+            PrecioVentaActual = precioVentaActual;
+            StockMinimo = stockMinimo;
+            Activo = activo;
+            IdCategoria = idCategoria;
+        }
+
+        public int IdProducto { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public decimal PrecioVentaActual { get; set; }
+        public int StockMinimo { get; set; }
+        public bool Activo { get; set; }
+        public int IdCategoria { get; set; }
+    }
+
+}
