09a27f745aa2ec483dfe33308493740ddd9cec27
FerDavalosHz <fernadodavalos2005@gmail.com>
2025-11-11 10:49:36 -0600

    Crud Proveedores

---
+++ Manejador/ManejadorProveedor.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+using AccesoDatos;
+using Entidades;
+ // Asegúrate de usar el namespace correcto
+
+namespace SistemaGestorPyme
+{
+    public class ManejadorProveedor
+    {
+
+
+        Base b = new Base("localhost", "root", "", "GestorPyme");
+
+        public void Guardar(Proveedor proveedor)
+        {
+        
+
+            b.Comando($"insert into tbl_proveedores(nombre, telefono, correo, direccion, activo) " +
+                      $"values('{proveedor.Nombre}', '{proveedor.Telefono}', '{proveedor.Correo}', '{proveedor.Direccion}', '{proveedor.Activo}')");
+
+            MessageBox.Show("Proveedor registrado con éxito.", "Guardar", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        public void Borrar(Proveedor proveedor)
+        {
+            var rs = MessageBox.Show($"¿Estás seguro de **dar de baja** el proveedor: {proveedor.Nombre}?",
+                                     "!Atención¡", MessageBoxButtons.YesNo, MessageBoxIcon.Warning); // Cambio a Warning ya que es una baja lógica
+            if (rs == DialogResult.Yes)
+            {
+  
+                b.Comando($"update tbl_proveedores set activo= 'inactivo' where id_proveedor={proveedor.IdProveedor}");
+
+                MessageBox.Show("Proveedor dado de baja con éxito (marcado como inactivo).", "Baja Lógica", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        public void Modificar(Proveedor proveedor)
+        {
+
+            b.Comando($"update tbl_proveedores set " +
+                      $"nombre='{proveedor.Nombre}', " +
+                      $"telefono='{proveedor.Telefono}', " +
+                      $"correo='{proveedor.Correo}', " +
+                      $"direccion='{proveedor.Direccion}', " +
+                      $"activo='{proveedor.Activo}' " +
+                      $"where id_proveedor={proveedor.IdProveedor}");
+
+            MessageBox.Show("Proveedor modificado con éxito.", "Modificar", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        public void Mostrar(string consulta, DataGridView tabla, string datos)
+        {
+            tabla.Columns.Clear();
+            tabla.DataSource = b.Consultar(consulta, datos).Tables[0];
+            
+                tabla.Columns["id_proveedor"].Visible = false;
+           
+            tabla.Columns.Insert(6, Boton("Modificar", Color.Green));
+            tabla.Columns.Insert(7, Boton("Eliminar", Color.Red));
+
+            
+            tabla.AutoResizeColumns();
+            tabla.AutoResizeRows();
+        }
+
+
+        private DataGridViewButtonColumn Boton(string titulo, Color fondo)
+        {
+            DataGridViewButtonColumn btn = new DataGridViewButtonColumn();
+            btn.Text = titulo;
+            btn.UseColumnTextForButtonValue = true;
+            btn.FlatStyle = FlatStyle.Popup;
+            btn.DefaultCellStyle.BackColor = fondo;
+            btn.DefaultCellStyle.ForeColor = Color.White;
+            return btn;
+        }
+    }
+}
---
+++ SistemaGestorPyME/FrmProveedor.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace SistemaGestorPyME
+{
+    partial class FrmProveedor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnAgregar = new System.Windows.Forms.Button();
+            this.BtnBuscar = new System.Windows.Forms.Button();
+            this.DtgDatos = new System.Windows.Forms.DataGridView();
+            this.TxtBuscar = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.DtgDatos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BtnAgregar
+            // 
+            this.BtnAgregar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnAgregar.Location = new System.Drawing.Point(455, 35);
+            this.BtnAgregar.Margin = new System.Windows.Forms.Padding(2);
+            this.BtnAgregar.Name = "BtnAgregar";
+            this.BtnAgregar.Size = new System.Drawing.Size(34, 24);
+            this.BtnAgregar.TabIndex = 41;
+            this.BtnAgregar.Text = "+";
+            this.BtnAgregar.UseVisualStyleBackColor = true;
+            this.BtnAgregar.Click += new System.EventHandler(this.BtnAgregar_Click_1);
+            // 
+            // BtnBuscar
+            // 
+            this.BtnBuscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnBuscar.Location = new System.Drawing.Point(380, 35);
+            this.BtnBuscar.Margin = new System.Windows.Forms.Padding(2);
+            this.BtnBuscar.Name = "BtnBuscar";
+            this.BtnBuscar.Size = new System.Drawing.Size(71, 24);
+            this.BtnBuscar.TabIndex = 40;
+            this.BtnBuscar.Text = "Buscar";
+            this.BtnBuscar.UseVisualStyleBackColor = true;
+            this.BtnBuscar.Click += new System.EventHandler(this.BtnBuscar_Click);
+            // 
+            // DtgDatos
+            // 
+            this.DtgDatos.AllowUserToAddRows = false;
+            this.DtgDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DtgDatos.Location = new System.Drawing.Point(46, 75);
+            this.DtgDatos.Margin = new System.Windows.Forms.Padding(2);
+            this.DtgDatos.Name = "DtgDatos";
+            this.DtgDatos.RowHeadersVisible = false;
+            this.DtgDatos.RowHeadersWidth = 51;
+            this.DtgDatos.RowTemplate.Height = 24;
+            this.DtgDatos.Size = new System.Drawing.Size(688, 340);
+            this.DtgDatos.TabIndex = 39;
+            this.DtgDatos.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DtgDatos_CellClick);
+            // 
+            // TxtBuscar
+            // 
+            this.TxtBuscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtBuscar.Location = new System.Drawing.Point(151, 35);
+            this.TxtBuscar.Margin = new System.Windows.Forms.Padding(2);
+            this.TxtBuscar.Name = "TxtBuscar";
+            this.TxtBuscar.Size = new System.Drawing.Size(225, 26);
+            this.TxtBuscar.TabIndex = 38;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(42, 35);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 20);
+            this.label1.TabIndex = 37;
+            this.label1.Text = "Proovedores";
+            // 
+            // FrmProveedor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BtnAgregar);
+            this.Controls.Add(this.BtnBuscar);
+            this.Controls.Add(this.DtgDatos);
+            this.Controls.Add(this.TxtBuscar);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmProveedor";
+            this.Text = "FrmProovedores";
+            this.Load += new System.EventHandler(this.FrmProveedor_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DtgDatos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnAgregar;
+        private System.Windows.Forms.Button BtnBuscar;
+        private System.Windows.Forms.DataGridView DtgDatos;
+        private System.Windows.Forms.TextBox TxtBuscar;
+        private System.Windows.Forms.Label label1;
+    }
+}
