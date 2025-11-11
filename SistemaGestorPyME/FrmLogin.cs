cad55c2d1a6fba4dafd6b492f24f784e85f8444b
ferISC5 <lm230110546@lagos.tecmm.edu.mx>
2025-11-11 11:10:58 -0600

    test

--- SistemaGestorPyME/FrmLogin.cs
+++ SistemaGestorPyME/FrmLogin.cs
@@ -12,6 +12,8 @@ namespace SistemaGestorPyME
 {
     public partial class FrmLogin : Form
     {
+        private Button button1;
+
         public FrmLogin()
         {
             InitializeComponent();
@@ -24,11 +26,22 @@ namespace SistemaGestorPyME
 
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(94, 97);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // FrmLogin
             // 
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button1);
             this.Name = "FrmLogin";
             this.Load += new System.EventHandler(this.FrmLogin_Load);
             this.ResumeLayout(false);
@@ -40,7 +53,7 @@ namespace SistemaGestorPyME
 
 
 
-            3
+            
         }
     }
 }
