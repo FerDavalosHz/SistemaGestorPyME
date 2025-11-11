0deaabafab14e2ebd0ee16fb03d6425ffc7b023a
Karla Perez <cecilia051122@gmail.com>
2025-11-11 09:41:25 -0600

    Creacion del Form de apertura de caja.

--- SistemaGestorPyME/FrmAtencion.Designer.cs
+++ SistemaGestorPyME/FrmAtencion.Designer.cs
@@ -42,13 +42,12 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Century Gothic", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(157, 68);
+            this.label1.Location = new System.Drawing.Point(150, 68);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(262, 84);
             this.label1.TabIndex = 0;
             this.label1.Text = "Se solicita contar el dinero en \r\n caja antes de iniciar el turno. \r\n       Asegú" +
     "rese de hacerlo \r\n           cuidadosamente";
-            //this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
